082b51081add0c2abe6a217c5a46e1597a21b283
Andrew Ma <comebacktj@yahoo.com>
2018-12-04 23:58:54 -0800

    initial commit

---
+++ src/main/java/worldsup/io/gifavideo/GifAVideoApplication.java
@@ -0,0 +1,40 @@
+package worldsup.io.gifavideo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+@SpringBootApplication
+public class GifAVideoApplication {
+	@Value("${multipart.location}/gif/")
+	private String gifLocation;
+	public static void main(String[] args) {
+		SpringApplication.run(GifAVideoApplication.class, args);
+	}
+	@PostConstruct
+	private void init() {
+		File gifFolder = new File(gifLocation);
+		if (!gifFolder.exists()) {
+			gifFolder.mkdir();
+		}
+	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+				registry.addResourceHandler("/gif/**")
+						.addResourceLocations("file:" + gifLocation);
+				super.addResourceHandlers(registry);
+			}
+		};
+	}
+}
